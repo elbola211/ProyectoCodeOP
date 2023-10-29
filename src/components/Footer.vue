b0cbc916c08d3d91dc2865f4e11bcecb30f91c28
Pasman211 <danielbola85@gmail.com>
2023-10-29 22:58:33 +0100

    PrimerReleaseHeaderFooterContacto

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,11 +1,31 @@
 <template>
-
+    <footer class="m-4 rounded-lg shadow w-screen border-solid border-2 border-indigo-600">
+        <!-- <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between"> -->
+        <div class="p-4 md:flex md:items-center md:justify-between">
+            <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400"> <img src=../IconosRRSS/nomade-digital2.jpg />
+            </span>
+            <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0 margin: 10px 10px">
+                <li class="p-4">
+                    <a href="#"><img src=../IconosRRSS/FB.jpg /></a>
+                </li>
+                <li class="p-4">
+                    <a href="#"><img src=../IconosRRSS/Insta.jpg /></a>
+                </li>
+                <li class="p-4">
+                    <a href="#"><img src=../IconosRRSS/Linkedin.jpg /></a>
+                </li>
+                <li class="p-4">
+                    <a href="#"><img src=../IconosRRSS/Wassap.jpg /></a>
+                </li>
+            </ul>
+        </div>
+    </footer>
 </template>
 
 <script>
-
+export default {
+  name: "Footer",  
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
