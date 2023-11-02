8c9de35579a3d2c9e7f742d4a890ddaf1d5e9f65
mauisiri <irisleo.com@gmail.com>
2023-11-02 22:04:26 +0100

    COMPLETED routes in main.js

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,24 @@
 <template>
+    <div>
+      <a href="https://vitejs.dev" target="_blank">
+        <img src="/vite.svg" class="logo" alt="Vite logo" />
+      </a>
+      <a href="https://vuejs.org/" target="_blank">
+        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+      </a>
+    </div>
+    <h1> Página principal Home</h1>
+  </template>
 
-</template>
+
 
 <script>
 
 </script>
 
+
+
 <style>
 
 </style>
+
