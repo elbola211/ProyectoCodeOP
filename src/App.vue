6ac1d4d16eb8cefed5629270bab0bc79b6330fbf
mauisiri <irisleo.com@gmail.com>
2023-11-03 16:22:16 +0100

    COMPLETED routes

--- src/App.vue
+++ src/App.vue
@@ -1,71 +1,15 @@
 
 <template>
-
-     
-
-    <!-- Contenedor para la NAV-->
-    <header>
-    <nav></nav>
-    </header>
-    
-    <!-- Contenedor para la Imagen -->
-  <div class="container mx-auto mt-2">
-    <div class="flex justify-between">
-      <a rel="noopener noreferrer" href="#" >
-    <img alt="Imagen1" public="./Brainstorming Creación página web Verde (1).jpg"/>
-   </a>
+ <div id='app'>
+    <div id='nav'>
+      <Home/>
+      <router-link to="/"> Home </router-link>
+      <router-link to="/contacto"> Contacto </router-link>
+      <router-link to="/destinos"> Destinations </router-link>
+      <router-link to="/infoutil"> infoUtil </router-link>
+      <router-link to="/opiniones"> Opiniones </router-link>
     </div>
+    <router-view/>
   </div>
-
-    <!-- Contenedor fin DE la Imagen -->
-
-    <div class="h-56 grid grid-cols-3 gap-4 content-center ...">
-    <div>¿Qué es ser un nómada digital?</div>
-    <div>Realidad del nómada digital</div>
-    <div>Ventajas e Incovenientes</div>
-    <div>Posibilidades para ser nomada digital</div>
-     </div>
-   
-
-    <!-- Contenedor para la Imagen y descripción-->
-
-
-  <div class="container max-w-6xl p-6 mx-auto space-y-6 sm:space-y-12">
-     <a rel="noopener noreferrer" href="#" class="block max-w-sm gap-3 mx-auto sm:max-w-full group hover:no-underline focus:no-underline lg:grid lg:grid-cols-12 dark:bg-gray-900">
-     <img class="object-cover w-full h-64 rounded sm:h-96 lg:col-span-7 dark:bg-gray-500" src="https://yes-tokio.es/wp-content/uploads/2020/11/tips-para-viajar-a-Tokio-980x400.jpg" alt=""/>
-     <div class="p-6 space-y-2 lg:col-span-5">
-  
-     <div class="text-sm font-semibold uppercase tracking-wide text-sky-500">TOKYO</div>
-    
-    <p class="mt-3 text-lsate-700">No hay duda de que Tokyo es una de las ciudades más fascinantes del Mundo, y una de las que más llama la atención por su mezcla de culturas asiáticas y occidentales.</p>
-    <p class="mt-3 text-lsate-700">Si eres amante de la tecnología, de seguro que Japón será una de las ciudades que te gustaría conocer, y es que, si alguna ciudad del planeta se parece a las representaciones ficticias del futuro del siglo pasado, esa es Tokio.</p>
-     </div>
-  </a>
-    </div>
-
-
-
-
 </template>
 
-
-<script>
-
-</script>
-
-
-  <!-- Contenedor para la Imagen<style>
-
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style> -->
--- src/views/Destinations.vue
+++ src/views/Destinations.vue
@@ -1,5 +1,7 @@
 <template>
-
+    <div class="destinations">
+      <h1> Página DESTINOS</h1>
+    </div>
 </template>
 
 <script>
