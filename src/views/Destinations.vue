6686098dcb7ec274cf31343605ed4792a4129aec
azaharacc <cerezo.azahara@gmail.com>
2023-11-07 18:41:32 +0100

    Pagina destinos nueva

--- src/views/Destinations.vue
+++ src/views/Destinations.vue
@@ -7,9 +7,9 @@
         <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4">
           <div v-for="destination in data" :key="destination.id">
             <div class="p-4">
-              <p class="text-gray-600 dark:text-gray-100 text-xl font-semibold">{{ destination.name }}</p>
               <router-link :to="{ name: 'destination-details', params: { id: destination.id } }">
                 <img :src="destination.image" alt="Imagen de {{ destination.name }}" class="w-full mb-2" />
+                <p class="text-gray-400 text-xl font-light">{{ destination.name }}</p>
               </router-link>
             </div>
 
