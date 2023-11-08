298d9bdb8ab4fb0d9511e12a8df3ea5dbbbeaba3
mauisiri <irisleo.com@gmail.com>
2023-11-08 22:59:42 +0100

    Added footer

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,19 @@
     <Navbar />
     <div id='nav'></div>
     <router-view/>
+    <Footer />
   </div>
-
 </template>
 
 <script>
 import Navbar from './components/Navbar.vue';
+import Footer from './components/Footer.vue';
 
 export default {
   name: 'app',
   components: {
-    'Navbar': Navbar
+    'Navbar': Navbar,
+    'Footer': Footer
   }
 }
 </script>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,31 +1,36 @@
 <template>
-    <nav class="shadow w-screen sticky top-0 z-50">
-      <div class="md:flex md:items-center md:left bg-slate-100 ">
-        <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400"> <img class="w-20 mr-12"  alt="Imagen1" src="../assets/logo/logo_img_text.jpg"/>
-            </span>
-        <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0 margin: 10px 10px">
-          <li class="p-4">
-              <h1 class="text-center text-xs font-semibold">
-              </h1>
-              <router-link to="/"> Inicio </router-link>
-          </li>
+  <nav class="shadow w-screen sticky top-0 z-50 mb-20">
+    <div class="md:flex md:items-center md:left bg-slate-100 ">
+      
+      <span>
+        <img class="w-20 mr-12" alt="LOGO"
+          src="../assets/logo/logo_img_text.jpg" />
+      </span>
+      
+      <ul
+        class="flex flex-wrap items-center text-lg font-medium text-gray-500 dark:text-gray-400 sm:mt-0 margin: 10px 10px">
+        <li class="p-4">
+          <h1 class="text-center text-xs font-semibold">
+          </h1>
+          <router-link to="/"> Inicio </router-link>
+        </li>
 
-          <li class="p-4">
-              <h1 class="text-center text-xs font-semibold "></h1>
-              <router-link to="/opiniones"> Opiniones </router-link>
-          </li>
+        <li class="p-4">
+          <h1 class="text-center text-xs font-semibold "></h1>
+          <router-link to="/opiniones"> Opiniones </router-link>
+        </li>
 
-          <li class="p-4">
-              <h1 class="text-center text-xs font-semibold"></h1>
-              <router-link to="/contacto"> Contacto </router-link>
-          </li>
-        </ul>
-      </div>
-    </nav>
+        <li class="p-4">
+          <h1 class="text-center text-xs font-semibold"></h1>
+          <router-link to="/contacto"> Contacto </router-link>
+        </li>
+      </ul>
+    </div>
+  </nav>
 </template>
 
 <script>
 export default {
-  name: "Navbar",  
+  name: "Navbar",
 };
 </script>
