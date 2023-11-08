76d9747b533e01726f68e6b862434e60363d7fd0
azaharacc <cerezo.azahara@gmail.com>
2023-11-08 19:38:17 +0100

    api tiempo

--- src/views/DestinationDetails.vue
+++ src/views/DestinationDetails.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="px-4 py-4" v-if="destination">
     <img :src="destination.image" alt="Imagen de {{ destination.name }}" class="w-1/2 mb-2" />   
+    <div v-if="weather">
+      <h3>Weather in {{ destination.name }}</h3>
+      <p>Temperature: {{ kelvinToCelsius(weather.main.temp) }}°C</p>
+      <p>Description: {{ weather.weather[0].description }}</p>
+      <p>Humidity: {{ weather.main.humidity }}%</p>
+      <p>Wind: {{ weather.wind.speed }} m/s</p>
+    </div>
   </div>
 
   <!-- Loading & Error Handling -->
@@ -11,6 +18,8 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: "DestinationDetails",
   data() {
@@ -18,6 +27,7 @@ export default {
       destination: null,
       loading: false,
       error: false,
+      weather: null,
     };
   },
   mounted() {
@@ -31,15 +41,27 @@ export default {
         const data = await response.json();
         this.loading = false;
         this.destination = data[this.$route.params.id];
-        console.log('Datos del destino:', this.destination); // Agrega este log para depurar
+        this.loadWeatherData(this.destination.name); // Cargar datos del tiempo al obtener los detalles del destino
       } catch (error) {
-        console.error('Error:', error); // Agrega este log para depurar
+        console.error('Error:', error); 
         this.loading = false;
         this.error = true;
       }
     },
-       imageLoadError() {
+    imageLoadError() {
       console.error("Error al cargar la imagen:", this.destination.image);
+    },
+    async loadWeatherData(city) {
+      try {
+        const apiKey = '0fc1f97fc35ae5c2fbfb17b51b0786ef'; // Reemplaza con tu clave de API de OpenWeatherMap
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`);
+        this.weather = response.data;
+      } catch (error) {
+        console.error('Error al obtener datos del tiempo:', error);
+      }
+    },
+    kelvinToCelsius(kelvin) {
+      return (kelvin - 273.15).toFixed(2);
     },
   },
 }
