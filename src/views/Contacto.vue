d2170d2a358e3cc808e6e3bff5d6602fb77bb415
mauisiri <irisleo.com@gmail.com>
2023-11-09 21:31:10 +0100

    Modifided contacto view

--- src/views/Contacto.vue
+++ src/views/Contacto.vue
@@ -1,22 +1,24 @@
 <template>
   <div class="contacto">
-  <div class="p-4 md:items-center">
-  <a href="#"><img src="../assets/Contacto.png" /></a>
-  <h2>Calle de Villarroel, 91, 08015, Barcelona</h2>
-  <a href="#"><h2>contacto@contacto.com</h2></a>
-  <a href="#"><h2>+34523418987</h2></a>
+    <div class="flex flex-wrap p-8 m-10">
+      <a class=rounded-md href="Mapa"><img class="rounded-md h-96" src="../assets/Contacto.png" /></a>
+
+      <div class="ml-10 mt-20 align-middle">
+        <h2 class="mb-4">Calle de Villarroel, 91, 08015, Barcelona</h2>
+
+        <h2 class="mb-4">hola@nomatech.com</h2>
+
+        <h2 >+34523418987</h2>
+
+      </div>
+    </div>
   </div>
-</div>
+</template>
   
+<script>
+export default {
+  name: "Contacto",
+};
+</script>
   
-  </template>
-  
-  <script>
-  export default {
-    name: "Contacto",  
-  };
-  </script>
-  
-  <style>
-  
-  </style>
+<style></style>
