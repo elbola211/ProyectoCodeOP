4fe17d3a451afef530851c6f499b104c990f35ba
neudysg <neudysgg@gmail.com>
2023-11-09 08:02:28 +0100

    Vista Jueves

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,55 +12,144 @@
 
     <!-- Contenedor fin DE la Imagen -->
 
-    <div class="h-20 my-1 bg-cyan-10 grid grid-cols-4 content-stretch...">
-    <div class="text-center text-xl mx-4 font-semibold uppercase tracking-wide text-sky-500 border-solid border-2">¿Qué es ser un nómada digital? </div>
-    <div class="text-center text-xl mx-4 font-semibold uppercase tracking-wide text-sky-500 border-solid border-2">Realidad del nómada digital</div>
-    <div class="text-center text-xl mx-4 font-semibold uppercase tracking-wide text-sky-500 border-solid border-2">Ventajas e Incovenientes</div> 
-    <div class="text-center text-xl mx-4 font-semibold uppercase tracking-wide text-sky-500 border-solid border-2">Posibilidades para ser nomada digital</div>
-     </div>
    
+      
+      <div class="grid grid-flow-col py-10 mx-20">
+        
+        <div class="mx-6 my-2 row-span-2">
+        <div class="text-2xl font-semibold uppercase tracking-wide text-sky-500">¿Qué es ser un nómada digital?</div>
+      
+         <p class="mt-2  text-xl text-lsate-700">El término “nómada digital” se refiere a personas que utilizan Internet 
+          para trabajar de forma remota mientras viajan de un lugar a otro, o sea, llevando una vida nómada. En lugar de estar en una ubicación fija, 
+          los nómadas digitales pueden realizar su trabajo desde cualquier lugar del mundo, siempre y cuando tengan acceso a Internet. </p>
+          <p class="mt-2  text-xl text-lsate-700">Pueden ser profesionales independientes, empresarios, freelancers o empleados que tienen la flexibilidad de realizar sus tareas 
+            desde cualquier ubicación con acceso a Internet.</p>
+        </div>
+        <div>
+          <img  class="object-cover rounded" src="https://www.semana.com/resizer/u8RHA2J5Osof1YtaV3RmL210wAk=/1280x720/smart/filters:format(jpg):quality(80)/cloudfront-us-east-1.images.arcpublishing.com/semana/EODGMMI3PRHAVOQL5RQPELZY5A.jpg" alt=""/>
+       </div>
+      </div>
+  
+
+
+
+ 
+      <div class="grid grid-rows-2 grid-flow-col  mx-20">
+        <div class="row-span-3">
+        <img class="object-cover w-full h-64 rounded sm:h-96" src="https://cvtrends.com/wp-content/uploads/2019/02/como-ser-nomada-digital.jpg" alt=""/>
+       </div>
+  
+        <div class="mx-6 row-span-3 col-span-2 ...">
+          <div class="my-1 mx-2 text-2xl font-semibold uppercase tracking-wide text-sky-500 ">Profesiones para nómadas digitales</div>
+       
+        <p class="mt-3 text-xl  mx-3 text-justify text-slate-700"> Hablemos ahora de las profesiones con las cuales puedes ser un nómada digital. 
+          Todas estas profesiones funcionan de manera digital, sea a un 100% o de manera parcial, 
+          pero de una manera que puedas trabajar 100% remoto.Puedes ser un nómada digital con las siguientes profesiones: </p>
+      
+          <p class="mt-3 text-xl  mx-3 text-justify text-slate-700 ">Diseñador gráfico,Desarrollador o diseñador web, Marketer digital,Especialista en SEO,Social media manager,Copywriter,
+            Creador de contenido, Project manager digital, Especialista en publicidad (SEM, Google Ads, o Facebook Ads),Asistente virtual,Analista web, Editor de textos,Fotógrafo,
+           Programador. </p>
+        </div>
+      </div>
+
+
+
+      <div class="grid grid-flow-col py-10 mx-20">
+        
+        <div class="mx-6 my-2 row-span-2">
+        <div class="text-2xl font-semibold uppercase tracking-wide text-sky-500">Ventajas de ser un nómada digital</div>
+      
+         <p class="mt-2  text-xl text-lsate-700">1- Libertad geográfica
+          Como nómada digital, tienes la libertad de elegir dónde quieres trabajar y vivir
+          Puedes viajar a diferentes lugares, explorar nuevos sitios mientras mantienes tu carrera profesional operativa. </p>
+          <p class="mt-2  text-xl text-lsate-700">2- Flexibilidad de horarios
+            Trabajar como nómada digital te permite establecer tus propios horarios de trabajo.
+            No estás limitado a un horario tradicional de 9 a 6, lo que te permite la libertad de organizar tu día de acuerdo con tus necesidades y preferencias personales.</p>
+          
+            <p class="mt-2  text-xl text-lsate-700">3- Equilibrio entre el trabajo y la vida personal
+              Al tener control sobre tu tiempo y ubicación, puedes conseguir un mejor equilibrio entre el trabajo y tu vida personal.
+              Por ejemplo, puedes organizar tu tiempo de trabajo para compaginarlo con tu vida familiar, tus hobbies o actividades deportivas sin perder tu ritmo de trabajo.</p>
+              
+              <p class="mt-2  text-xl text-lsate-700">4- Desarrollo personal
+                Vivir como nómada digital implica enfrentar desafíos constantes y adaptarse a entornos cambiantes.
+                Esto ayuda el desarrollo personal, el aprendizaje de nuevas habilidades, a toma de decisiones y a la capacidad de adaptarte a diferentes situaciones.</p>
+                <p class="mt-2  text-xl text-lsate-700">5-Amplia red de contactos
+                  Con este estilo de vida nomada tendrás la oportunidad de conectarte con personas de todo el mundo.
+                  Conocerás a otros profesionales, emprendedores y viajeros, lo que te permitirá ampliar tu red de contactos y quizás a nuevas oportunidades de colaboración y crecimiento.</p>
+
+        </div>
+        <div>
+          <img  class="h-90 object-cover rounded" src="https://crehana-blog.imgix.net/media/filer_public/34/5f/345f1742-4618-4ef0-a98c-b51d0b6d6f93/que_busca_un_nomada_digital.jpg?auto=format&q=50" alt=""/>
+       </div>
+      </div>
+
+
+   
+      <div class="grid grid-rows-2 grid-flow-col  mx-20">
+        <div class="row-span-3">
+        <img class="object-cover w-full h-64 rounded sm:h-96" src="https://lexdocuments.com/wp-content/uploads/2022/06/Visa-nomada-digital-1536x768.jpg" alt=""/>
+       </div>
+  
+        <div class="mx-6 row-span-3 col-span-2 ...">
+          <div class="my-1 mx-2 text-2xl font-semibold uppercase tracking-wide text-sky-500 ">Posibilidades para ser nomada digital</div>
+       
+          <p class="mt-3 text-xl  mx-3 text-justify text-slate-700"> Como ha quedado claro, no puedes ser un nómada digital manteniendo un trabajo tradicional. Por lo cual, hay tres maneras en los que tu profesión te permitirá trabajar por el mundo: </p>
+          <p class="mt-3 text-xl  mx-3 text-justify text-slate-700">1. Siendo un freelancer: ya que en este modalidad te pagan por trabajo entregado más que por cumplir horarios. Así que va perfecto para el nómada digital. </p>
+          <p class="mt-3 text-xl  mx-3 text-justify text-slate-700">2. Teniendo una empresa propia: en especial un negocio digital que te permita trabajar a distancia. </p>
+          <p class="mt-3 text-xl  mx-3 text-justify text-slate-700">3. Trabajando de manera remota: en la actualidad hay startups y empresas que funcionan 100% remoto y que te facilitarán la vida para trabajar de esta manera. </p>
+        </div>
+      </div>
+
+
 
     <!-- Contenedor para la Imagen y descripción-->
 
 
-    <div class="grid grid-rows-2 grid-flow-col gap-4 py-4 mx-9">
+    <div class="grid grid-rows-2 grid-flow-col gap-4 py-20 mx-20">
       <div class="row-span-3">
-      <img class="object-cover w-full h-64 rounded sm:h-96" src="https://yes-tokio.es/wp-content/uploads/2020/11/tips-para-viajar-a-Tokio-980x400.jpg" alt=""/>
+      <img class="object-cover w-full h-64 rounded sm:h-96" src="https://static-uat.cambiocolombia.com/s3fs-public/styles/style_aspect_2_3_large/public/2023-09/mujeres-turistas-mano-tienen-feliz-mapa-viaje.jpg.webp?itok=IVWtNFK7" alt=""/>
      </div>
 
       <div class="mx-6 row-span-3 col-span-2 ...">
-      <div class="text-2xl font-semibold uppercase tracking-wide text-sky-500">TOKYO</div>
+      <div class="text-2xl font-semibold uppercase tracking-wide text-sky-500">Mejores países para nómadas digitales</div>
      
-       <p class="mt-3 text-lsate-700">No hay duda de que Tokyo es una de las ciudades más fascinantes del Mundo, y una de las que más llama la atención por su mezcla de culturas asiáticas y occidentales.</p>
-       <p class="mt-3 text-lsate-700">Tokio es la capital de Japón y el mejor lugar desde donde comenzar a explorar el país. Prepárese para el ajetreo y el bullicio: Tokio es una de las ciudades más pobladas del mundo, con más de 13 millones de personas viviendo solo en la ciudad y más de 37 millones en el área metropolitana de Tokio. Esta emocionante ciudad es tan ecléctica como el mismo Japón y verá edificios conmovedores que albergan gigantes tecnológicos del país junto a edificios tradicionales y antiguas calles estrechas.</p>
-       <p class="mt-3 text-lsate-700">Si eres amante de la tecnología, de seguro que Japón será una de las ciudades que te gustaría conocer, y es que, si alguna ciudad del planeta se parece a las representaciones ficticias del futuro del siglo pasado, esa es Tokio.</p>
+       <p class="mt-3 text-xl text-slate-700">Elegir a dónde ir como nómada digital es muy importante porque se tienen necesidades determinadas: lugares cómodos y con buena conexión a internet para poder desarrollarse. </p>
+       <p class="mt-3 text-xl text-slate-700">Por ello y con el fin de ayudarte, te vamos a dejar un top de los mejores países para nómadas digitales. 
+        No tienes que elegir uno de estos, pero pueden ser excelentes opciones si quieres iniciar o si ya tienes carrera en esto.</p>
+        <p class="mt-3 text-xl text-slate-700">Tailandia: Bangkok, Chiang Mai y Ko Pha Ngan.
+          Indonesia: Canggu (Bali), Ubud (Bali) y Seminyak (Bali).
+          República Checa: Praga y Brno.
+          Hungría: Budapest.
+          Portugal: Lisboa, Madeira, Porto y Ericeira.
+          Georgia: Tbilisi.
+          México: Ciudad de México.
+          España: Tenerife, Las Palmas, Barcelona, Madrid y Valencia.
+          Rusia: San Petersburgo y Moscú.
+          Polonia: Varsovia, Cracovia, Wrocław, Poznan y Gdansk.</p>
       </div>
     </div>
   
 
 
-    <div class="grid grid-rows-2 grid-flow-col gap-4 py-4 mx-9">
+    <div class="grid grid-rows-2 grid-flow-col gap-4 py-4 mx-20">
       <div class="row-span-3">
       <img class="object-cover w-full h-64 rounded sm:h-96" src="https://www.aytolalaguna.es/CDN/files/nomadas-digitales/es/blog/.content/imagenes_blog/consejos-tratar-clientes-distancia.jpg_1515856084.jpg" alt=""/>
      </div>
 
       <div class="mx-6 row-span-3 col-span-2 ...">
       <div class="text-2xl font-semibold uppercase tracking-wide text-sky-500">CONSEJO PRÁCTICO PARA UN NÓMADA DIGITAL</div>
-     
-       <p class="mt-3  text-lsate-700">Sigue un horario...</p>
-       <p class="mt-3  text-lsate-700">Que no trabajes en una oficina o que seas tu propio jefe (el caso de muchos nómadas digitales) no significa que 
-        carezcas de horarios. Planificar tu jornada de trabajo es esencial para ser productivo y sacar el máximo provecho
-         al tiempo que pasas frente al ordenador. </p>
-       <p class="mt-3  text-lsate-700">No tiene por qué ser un horario rígido todas las semanas ni siquiera todos los días, dependerá de la carga de trabajo 
-        y del lugar del mundo donde estés en cada momento. Pero sí puedes organizarte con cierta antelación. 
-        Para eso, también, te vendrá muy bien tu agenda digital.</p>
-       <p class="mt-3  text-lsate-700">En general, para trabajar como nómada digital de manera organizada necesitas diferenciar el tiempo de trabajo del de ocio. 
-        De esta forma te centrarás solo en lo que toca y disfrutarás más de la experiencia de 
-        ejercer tu profesión mientras viajas por el mundo.¡Suerte!</p>
+       <p class="mt-3  text-xl text-lsate-700">Sigue un horario...</p>
+       <p class="mt-3  text-xl text-lsate-700">Que no trabajes en una oficina o que seas tu propio jefe (el caso de muchos nómadas digitales) no significa que 
+        carezcas de horarios. Planificar tu jornada de trabajo es esencial para ser productivo y sacar el máximo provecho al tiempo que pasas frente al ordenador. </p>
+       <p class="mt-3  text-xl text-lsate-700">No tiene por qué ser un horario rígido todas las semanas ni siquiera todos los días, dependerá de la carga de trabajo 
+        y del lugar del mundo donde estés en cada momento. Pero sí puedes organizarte con cierta antelación.  Para eso, también, te vendrá muy bien tu agenda digital.</p>
+       <p class="mt-3  text-xl text-lsate-700">En general, para trabajar como nómada digital de manera organizada necesitas diferenciar el tiempo de trabajo del de ocio. 
+        De esta forma te centrarás solo en lo que toca y disfrutarás más de la experiencia de ejercer tu profesión mientras viajas por el mundo.¡Suerte!</p>
       </div>
     </div>
 
-    <div class="grid grid-rows-2 grid-flow-col gap-4 py-4 mx-9">
+
+    <div class="grid grid-rows-2 grid-flow-col gap-4 py-4 mx-20">
       <div class="row-span-3">
       <img class="object-cover w-full h-64 rounded sm:h-96" src="https://www.escribir.me/wp-content/uploads/2019/04/DSC07971.jpg" alt=""/>
      </div>
@@ -68,13 +157,13 @@
       <div class="mx-6 row-span-3 col-span-2 ...">
       <div class="text-2xl font-semibold uppercase tracking-wide text-sky-500">EXPERIENCIA DE UNA NÓMADA DIGITAL</div>
      
-       <p class="text-2xl my-2  text-lsate-700">ANIKO VILLALBA</p>
-       <p class="mt-3  text-lsate-700">Esta nómada digital entre el 2008 y el 2018 vivió viajando, y creó un blog que fue su diario íntimo </p>
-       <p class="mt-3  text-lsate-700">Durante 10 años se dediquó a la escritura y fotografía de viajes. Durante esa década viajó sola por más 
+       <p class="text-2xl my-2 text-lsate-700">ANIKO VILLALBA</p>
+       <p class="mt-3  text-xl text-lsate-700">Esta nómada digital entre el 2008 y el 2018 vivió viajando, y creó un blog que fue su diario íntimo </p>
+       <p class="mt-3  text-xl text-lsate-700">Durante 10 años se dediquó a la escritura y fotografía de viajes. Durante esa década viajó sola por más 
         de 50 países y escribió acerca de su experiencias en revistas, periódicos, sitios web y mi blog de viajes (viajandoporahi.com). 
         También dio charlas y conferencias, publiquó su primeros dos libros de narrativa de viajes y empezó a crear diarios interactivos de viajes con consignas, 
         disparadores y preguntas para completar por el lector.</p>
-       <p class="mt-3  text-lsate-700">Comenta que su experiencia durante esos 10 año fueron enriquecedora,ya que la ayudo a conocerse ella misma, se más creativa e independiente, 
+       <p class="mt-3  text-xl text-lsate-700">Comenta que su experiencia durante esos 10 año fueron enriquecedora,ya que la ayudo a conocerse ella misma, se más creativa e independiente, 
         altualmente ayuda a las personas a conectarse con ellas misma y con su creatividad y con sus mundos internos.  </p>
       </div>
     </div>
@@ -84,8 +173,8 @@
 
  
  
+  </div>
 
-</div>
 
 </template>
 
