d3f3a4965026cbfc7fbea163b040ccc517140647
neudysg <neudysgg@gmail.com>
2023-11-09 15:21:34 +0100

    Vista Jueves container azul

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -84,12 +84,42 @@
       </div>
 
 
-   
+
       <div class="grid grid-rows-2 grid-flow-col  mx-20">
+        <div class="row-span-3">
+        <img class="object-cover w-full h-64 rounded sm:h-96" src="https://www.debate.com.mx/__export/1675985367123/sites/debate/img/2023/02/09/pexels-vlada-karpovich-4050320.jpg_347796135.jpg" alt=""/>
+       </div>
+  
+        <div class="mx-6 row-span-3 col-span-2 ...">
+          <div class="my-1 mx-2 text-2xl font-semibold uppercase tracking-wide text-sky-500 "> Desventajas de ser un nómada digital</div>
+       
+        <p class="mt-3 text-xl  mx-3 text-justify text-slate-700"> No obstante, algunas desventajas habituales al estilo de vida de un nómada digital son: </p>
+      
+          <p class="mt-3 text-xl  mx-3 text-justify text-slate-700 ">1- La diferencia horaria con respecto a tus clientes. </p>
+          <p class="mt-3 text-xl  mx-3 text-justify text-slate-700 ">2- La necesidad de tener Internet en países en los que la red todavía no se encuentra demasiado desarrollada. </p>
+          <p class="mt-3 text-xl  mx-3 text-justify text-slate-700 ">3- Los cortes de luz y otros imprevistos que pueden jugarte una mala pasada. </p>
+          <p class="mt-3 text-xl  mx-3 text-justify text-slate-700 ">4- En ocasiones, los trámites de fiscalidad, que se pueden solucionar con la ayuda de un gestor. </p>
+        </div>
+      </div>
+
+   <!-- Contenedor fin para la Imagen y descripción-->
+
+    <div class="grid grid-rows-2 grid-flow-col gap-4 py-20 mx-20">
+      <div class="row-span-3">
+      <img class="object-cover w-full h-64 rounded sm:h-96" src="https://fundacionuniversidadempresa.es/wp-content/uploads/2021/03/nomadas-digitales-1024x700.jpg" alt=""/>
+     </div>
+  </div>
+  
+
+ <!-- Contenedor inicio la Imagen y experiencia-->
+
+ <div class="container mx-auto bg-blue-300">
+  
+
+      <div class="grid grid-rows-2 grid-flow-col py-4 mx-20">
         <div class="row-span-3">
         <img class="object-cover w-full h-64 rounded sm:h-96" src="https://lexdocuments.com/wp-content/uploads/2022/06/Visa-nomada-digital-1536x768.jpg" alt=""/>
        </div>
-  
         <div class="mx-6 row-span-3 col-span-2 ...">
           <div class="my-1 mx-2 text-2xl font-semibold uppercase tracking-wide text-sky-500 ">Posibilidades para ser nomada digital</div>
        
@@ -100,35 +130,7 @@
         </div>
       </div>
 
-
-
-    <!-- Contenedor para la Imagen y descripción-->
-
-
-    <div class="grid grid-rows-2 grid-flow-col gap-4 py-20 mx-20">
-      <div class="row-span-3">
-      <img class="object-cover w-full h-64 rounded sm:h-96" src="https://static-uat.cambiocolombia.com/s3fs-public/styles/style_aspect_2_3_large/public/2023-09/mujeres-turistas-mano-tienen-feliz-mapa-viaje.jpg.webp?itok=IVWtNFK7" alt=""/>
-     </div>
-
-      <div class="mx-6 row-span-3 col-span-2 ...">
-      <div class="text-2xl font-semibold uppercase tracking-wide text-sky-500">Mejores países para nómadas digitales</div>
-     
-       <p class="mt-3 text-xl text-slate-700">Elegir a dónde ir como nómada digital es muy importante porque se tienen necesidades determinadas: lugares cómodos y con buena conexión a internet para poder desarrollarse. </p>
-       <p class="mt-3 text-xl text-slate-700">Por ello y con el fin de ayudarte, te vamos a dejar un top de los mejores países para nómadas digitales. 
-        No tienes que elegir uno de estos, pero pueden ser excelentes opciones si quieres iniciar o si ya tienes carrera en esto.</p>
-        <p class="mt-3 text-xl text-slate-700">Tailandia: Bangkok, Chiang Mai y Ko Pha Ngan.
-          Indonesia: Canggu (Bali), Ubud (Bali) y Seminyak (Bali).
-          República Checa: Praga y Brno.
-          Hungría: Budapest.
-          Portugal: Lisboa, Madeira, Porto y Ericeira.
-          Georgia: Tbilisi.
-          México: Ciudad de México.
-          España: Tenerife, Las Palmas, Barcelona, Madrid y Valencia.
-          Rusia: San Petersburgo y Moscú.
-          Polonia: Varsovia, Cracovia, Wrocław, Poznan y Gdansk.</p>
-      </div>
-    </div>
-  
+    
 
 
     <div class="grid grid-rows-2 grid-flow-col gap-4 py-4 mx-20">
@@ -138,12 +140,12 @@
 
       <div class="mx-6 row-span-3 col-span-2 ...">
       <div class="text-2xl font-semibold uppercase tracking-wide text-sky-500">CONSEJO PRÁCTICO PARA UN NÓMADA DIGITAL</div>
-       <p class="mt-3  text-xl text-lsate-700">Sigue un horario...</p>
-       <p class="mt-3  text-xl text-lsate-700">Que no trabajes en una oficina o que seas tu propio jefe (el caso de muchos nómadas digitales) no significa que 
+       <p class="mt-3 mx-3 text-xl text-slate-700">Sigue un horario...</p>
+       <p class="mt-3 mx-3 text-xl text-slate-700">Que no trabajes en una oficina o que seas tu propio jefe (el caso de muchos nómadas digitales) no significa que 
         carezcas de horarios. Planificar tu jornada de trabajo es esencial para ser productivo y sacar el máximo provecho al tiempo que pasas frente al ordenador. </p>
-       <p class="mt-3  text-xl text-lsate-700">No tiene por qué ser un horario rígido todas las semanas ni siquiera todos los días, dependerá de la carga de trabajo 
+       <p class="mt-3 mx-3 text-xl text-slate-700">No tiene por qué ser un horario rígido todas las semanas ni siquiera todos los días, dependerá de la carga de trabajo 
         y del lugar del mundo donde estés en cada momento. Pero sí puedes organizarte con cierta antelación.  Para eso, también, te vendrá muy bien tu agenda digital.</p>
-       <p class="mt-3  text-xl text-lsate-700">En general, para trabajar como nómada digital de manera organizada necesitas diferenciar el tiempo de trabajo del de ocio. 
+       <p class="mt-3  mx-3 text-xl text-slate-700">En general, para trabajar como nómada digital de manera organizada necesitas diferenciar el tiempo de trabajo del de ocio. 
         De esta forma te centrarás solo en lo que toca y disfrutarás más de la experiencia de ejercer tu profesión mientras viajas por el mundo.¡Suerte!</p>
       </div>
     </div>
@@ -157,21 +159,46 @@
       <div class="mx-6 row-span-3 col-span-2 ...">
       <div class="text-2xl font-semibold uppercase tracking-wide text-sky-500">EXPERIENCIA DE UNA NÓMADA DIGITAL</div>
      
-       <p class="text-2xl my-2 text-lsate-700">ANIKO VILLALBA</p>
-       <p class="mt-3  text-xl text-lsate-700">Esta nómada digital entre el 2008 y el 2018 vivió viajando, y creó un blog que fue su diario íntimo </p>
-       <p class="mt-3  text-xl text-lsate-700">Durante 10 años se dediquó a la escritura y fotografía de viajes. Durante esa década viajó sola por más 
+       <p class="text-2xl my-2 text-slate-700">ANIKO VILLALBA</p>
+       <p class="mt-3 mx-3 text-xl text-slate-700">Esta nómada digital entre el 2008 y el 2018 vivió viajando, y creó un blog que fue su diario íntimo </p>
+       <p class="mt-3 mx-3 text-xl text-slate-700">Durante 10 años se dediquó a la escritura y fotografía de viajes. Durante esa década viajó sola por más 
         de 50 países y escribió acerca de su experiencias en revistas, periódicos, sitios web y mi blog de viajes (viajandoporahi.com). 
         También dio charlas y conferencias, publiquó su primeros dos libros de narrativa de viajes y empezó a crear diarios interactivos de viajes con consignas, 
         disparadores y preguntas para completar por el lector.</p>
-       <p class="mt-3  text-xl text-lsate-700">Comenta que su experiencia durante esos 10 año fueron enriquecedora,ya que la ayudo a conocerse ella misma, se más creativa e independiente, 
+       <p class="mt-3 mx-3 text-xl text-slate-700">Comenta que su experiencia durante esos 10 año fueron enriquecedora,ya que la ayudo a conocerse ella misma, se más creativa e independiente, 
         altualmente ayuda a las personas a conectarse con ellas misma y con su creatividad y con sus mundos internos.  </p>
       </div>
     </div>
 
 
+    <div class="grid grid-rows-2 grid-flow-col gap-4 py-4 mx-20">
+      <div class="row-span-3">
+      <img class="object-cover w-full h-64 rounded sm:h-96" src="https://tiempodenegocios.com/wp-content/uploads/2017/03/viajes.jpg" alt=""/>
+     </div>
 
+      <div class="mx-6 row-span-3 col-span-2 ...">
+      <div class="text-2xl font-semibold uppercase tracking-wide text-sky-500">MEJORES PAÍSES PARA NÓMADAS DIGITALES</div>
+       
+      <p class="mt-3 mx-3 text-xl text-slate-700">Elegir a dónde ir como nómada digital es muy importante porque se tienen necesidades determinadas: lugares cómodos y con buena conexión a internet para poder desarrollarse. </p>
+       <p class="mt-3 mx-3 text-xl text-slate-700">Por ello y con el fin de ayudarte, te vamos a dejar un top de los mejores países para nómadas digitales. 
+        No tienes que elegir uno de estos, pero pueden ser excelentes opciones si quieres iniciar o si ya tienes carrera en esto.</p>
+        <p class="mt-3 mx-3 text-xl text-slate-700">Tailandia: Bangkok, Chiang Mai y Ko Pha Ngan.
+          Indonesia: Canggu (Bali), Ubud (Bali) y Seminyak (Bali).
+          República Checa: Praga y Brno.
+          Hungría: Budapest.
+          Portugal: Lisboa, Madeira, Porto y Ericeira.
+          Georgia: Tbilisi.
+          México: Ciudad de México.
+          España: Tenerife, Las Palmas, Barcelona, Madrid y Valencia.
+          Rusia: San Petersburgo y Moscú.
+          Polonia: Varsovia, Cracovia, Wrocław, Poznan y Gdansk.</p>
+      
+      </div>
+    
+    </div>
+    
+  </div>   
 
- 
  
   </div>
 
